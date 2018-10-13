4d1be8086f9c70ba14003b0f36b08ed6eb07da84
Joseca <jcampino1@uc.cl>
2018-10-13 11:09:12 -0300

    Listo para VN

--- programa1.py
+++ programa1.py
@@ -12,8 +12,9 @@ class Corte:
         utilidad_corte = 0
         for pieza, cantidad in self.dicc_piezas.items():
             utilidad_corte += pieza.calcular_coef(cantidad, valor_metro_astillado)
-            #print('{0}: {1}, costo={2}'.format(self.numero, utilidad_corte, self.costo))
-        #time.sleep(2)
+            #if self.numero == 1:
+                #print('Patron de corte {0}, agregando {1} de la pieza {2}. Utilidad:{3}'.format(self.numero, cantidad, pieza.indice, utilidad_corte))
+                #time.sleep(2)
 
         return self.metros_sobrantes*valor_metro_astillado - self.costo + utilidad_corte
 
@@ -31,13 +32,16 @@ class Pieza:
     def calcular_coef(self, unidades_obtenidas, valor_astillado_largo):
         cantidad_final = self.stock + unidades_obtenidas
         u1 = (self.lista_coef[0]/self.lista_coef[1])*self.stock - (1/self.lista_coef[1])*self.stock**2
-        #print(u1)
         u2 = (self.lista_coef[0]/self.lista_coef[1])*cantidad_final - (1/self.lista_coef[1])*cantidad_final**2
-        #print(u2)
+
+        # No se si esta bien poner esto, el maximo entre u2 y 0
         u2 = max(u2, 0)
-        #print(u2 - u1)
-        #time.sleep(20)
-        #return u2 - u1
+
+        #if self.indice == 3:
+        #    print('Stock pieza: {0}, agregado: {1}. Utilidad: {2}'.format(self.stock, unidades_obtenidas, u2 - u1))
+        #    time.sleep(5)
+
+        # Hay que darle vueltas a esto tambien
         return max(u2-u1, self.largo*valor_astillado_largo)
 
 
@@ -48,10 +52,15 @@ class Dia:
         self.lista_inventario = lista_inventario
         self.valor_metro_astillado = valor_metro_astillado
         self.costo_total = 0
+        self.utilidad_total = 0
         self.dicc_patrones_usados = {}
+        self.stock_piezas = {}
 
         for i in range (0, len(self.dicc_patrones.keys())):
             self.dicc_patrones_usados[i+1] = 0
+
+        for i in range(0, 10):
+            self.stock_piezas[i+1] = 0
 
 
     def elegir_corte(self):
@@ -59,7 +68,6 @@ class Dia:
         corte_max = None
         for patron in self.dicc_patrones.values():
             coef = patron.calcular_coef(self.valor_metro_astillado)
-            print('{0} final: {1}'.format(patron.numero, coef))
             if coef >= maximo:
                 maximo = coef
                 corte_max = patron
@@ -73,12 +81,18 @@ class Dia:
             self.costo_total += corte.costo
             for pieza, cantidad in corte.dicc_piezas.items():
                 pieza.stock += cantidad
-                print('Stock de la pieza {0}: {1}'.format(pieza.indice, pieza.stock))
+                self.stock_piezas[pieza.indice] += cantidad
+                self.utilidad_total += coef
+                #print('Stock de la pieza {0}: {1}'.format(pieza.indice, pieza.stock))
+
             self.dicc_patrones_usados[corte.numero] += 1
-            print("El corte elegido es el {0}".format(corte.numero))
+            #print("El corte elegido es el {0}".format(corte.numero))
+
             num_troncos -= 1
             #time.sleep(0.5)
         print(self.dicc_patrones_usados.values())
+        print(self.stock_piezas.values())
+        print('Utilidad total: {0}, costo total: {1}'.format(self.utilidad_total, self.costo_total))
 
 piezas = dict()
 # Diccionario de la forma indice: Pieza()
@@ -101,6 +115,7 @@ for linea in archivo2:
     patrones[int(linea[0][1:])] = Corte(int(linea[0][1:]), int(linea[1]), diccionario_piezas, int(linea[11]))
 archivo2.close()
 
+# Por ahora no hay inventario
 inventario = None
 metro_astillado = 2050
 
